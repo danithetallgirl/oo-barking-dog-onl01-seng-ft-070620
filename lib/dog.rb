3d83f0ae1ea095b456e0aa1ad3459691e505c09e
Dani Bassett <dani@danithetallgirl.com>
2020-07-14 00:47:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,21 @@
 class Dog
-  def 
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+  
+  def name
+    @this_dogs_name
+  end
 end
 
-
 fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.name
 
 fido.name
  
